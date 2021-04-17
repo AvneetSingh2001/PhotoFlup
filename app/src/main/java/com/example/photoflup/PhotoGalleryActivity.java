7f47a4b7ffb58c53af32e31f9b5659b20edacfae
DCK <avneetmaankiya@gmail.com>
2021-04-18 02:57:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/photoflup/PhotoGalleryActivity.java
@@ -0,0 +1,24 @@
+package com.example.photoflup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
+
+
+    @Override
+    public Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
+    }
+
+
+}
